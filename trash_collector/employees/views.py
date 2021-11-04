0c76dfe59dd37491a4582defc49915295f5b9d77
andrew morales <andrew.morales725@gmail.com>
2021-11-04 15:42:38 -0400

    Added employee registration

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,47 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.apps import apps
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Employee
+from datetime import date
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
 
-def index(request):
+def get_customer(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+def index(request):
+    # The following line will get the logged-in user (if there is one) within any view function
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        return render(request, 'employees/create.html')
